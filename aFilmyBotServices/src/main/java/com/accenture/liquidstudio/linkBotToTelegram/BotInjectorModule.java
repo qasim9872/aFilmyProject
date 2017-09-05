17eca0f5bf7c6ae2177a97f2e84e73bdc73f1944
Muhammad Qasim <muhammad.qasim@accenture.com>
2017-09-05 23:08:08 +0100

    api keys

--- aFilmyBotServices/src/main/java/com/accenture/liquidstudio/linkBotToTelegram/BotInjectorModule.java
+++ aFilmyBotServices/src/main/java/com/accenture/liquidstudio/linkBotToTelegram/BotInjectorModule.java
@@ -9,27 +9,31 @@ import com.google.inject.name.Names;
 
 public class BotInjectorModule extends AbstractModule {
 
-//	private final String apiaiKey = "f708da8c8f974d3f9bc7e9c22d759c5a";
-	private final String botUsername = "AFilmyFirstTryBot";
+	//
+	private final String botUsername = "<INSERT BOT USERNAME HERE>";
+
+	// The key below is the consumer key, you can train your agent and use that key
+	// or use the provided one
 	private final String apiaiKey = "0e856cc6394f43e1bc34214dd9aa876d";
-	private final String tmdbAPiKey = "9bd86e6a97f659f0484a59ffb93a1858";
-	private final String botToken = "376265490:AAG3IS9bTpiQHSnnOpGZ2JTstUohNDzqXBw";
 	
 	
+	private final String tmdbAPiKey = "<INSERT TMDB API KEY HERE>";
+	private final String botToken = "<INSERT BOT TOKEN HERE>";
+
 	@Override
 	protected void configure() {
 		// TODO Auto-generated method stub
-		
+
 		bind(ReplyService.class).to(ParseIntentAndReplyService.class);
 		bind(IAccessTmdbApi.class).to(AccessTmdbApi.class);
-		
-		
-		//Binding all the keys to named constants in order to initialize the classes using dependency injector
+
+		// Binding all the keys to named constants in order to initialize the classes
+		// using dependency injector
 		bindConstant().annotatedWith(Names.named("apiaiKey")).to(apiaiKey);
 		bindConstant().annotatedWith(Names.named("tmdbApiKey")).to(tmdbAPiKey);
 		bindConstant().annotatedWith(Names.named("botToken")).to(botToken);
 		bindConstant().annotatedWith(Names.named("botUsername")).to(botUsername);
-		
+
 	}
 
 }
